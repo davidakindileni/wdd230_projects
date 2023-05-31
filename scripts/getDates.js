1ec002bc41aa640b72abe9208d1594ae78d7cd90
davidakindileni <davidakindileni@yahoo.co.uk>
2023-05-31 17:28:53 +0100

    first project upload

---
+++ scripts/getDates.js
@@ -0,0 +1,6 @@
+// Set the year for copyright
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+
+// Set the date when last updated
+let a = document.lastModified;
+document.getElementById("lastmodified").innerHTML = a;
