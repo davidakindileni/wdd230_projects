b8d3dedef536853ce751a5f542cc11f269ca9342
davidakindileni <davidakindileni@yahoo.co.uk>
2023-06-17 07:58:41 +0100

    w02 changes

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,3 +4,19 @@ document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 // Set the date when last updated
 let a = document.lastModified;
 document.getElementById("lastmodified").innerHTML = a;
+
+// Dark Mode script
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+  if (modeButton.textContent.includes("☑️")) {
+    main.style.background = "#000";
+    main.style.color = "#fff";
+    modeButton.textContent = "❎";
+  } else {
+    main.style.background = "#eee";
+    main.style.color = "#000";
+    modeButton.textContent = "☑️";
+  }
+});
