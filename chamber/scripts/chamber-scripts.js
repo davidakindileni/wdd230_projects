aa54ea4babb2b6edf208949654a2e454df9dc893
davidakindileni <davidakindileni@yahoo.co.uk>
2023-07-07 19:53:22 +0100

    W03 assignments completed

--- chamber/scripts/chamber-scripts.js
+++ chamber/scripts/chamber-scripts.js
@@ -14,16 +14,37 @@ hamButton.addEventListener("click", () => {
   hamButton.classList.toggle("open");
 });
 
-/* Check for the active navigation button */
-var navi = document.getElementById("nav-bar");
-var links = navi.getElementsByClassName("navlink");
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener(
-    "click",
-    ready(function () {
-      var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
-    })
-  );
+// get number of days since last visit
+// 1️⃣ Initialize display element variable
+const lastVisit = document.querySelector(".lastVisit");
+const lastVisitDate = window.localStorage.getItem("storedLastVisit");
+
+if (lastVisitDate == null) {
+  // consider first visit
+  lastVisit.textContent = `Welcome! 🥳 Let us know if you have any questions.`;
+  localStorage.setItem("storedLastVisit", JSON.stringify(new Date())); // your choice of format
+} else {
+  const lastVisited = localStorage.getItem("storedLastVisit");
+  // your logic to calculate days based on format stored.
+  // const lastDate = JSON.parse(lastVisited); //
+  lastDate = Date.parse(JSON.parse(lastVisited));
+  const currentDate = new Date();
+  const nowDate = Date.parse(currentDate);
+  const difference = nowDate - lastDate;
+  const differenceInDays = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+  // lastVisit.textContent = differenceInDays; //
+  if (differenceInDays < 1) {
+    lastVisit.textContent = `Back so soon! Awesome!`;
+  }
+
+  if (differenceInDays == 1) {
+    textEnd = ` day ago.`;
+  }
+
+  if (differenceInDays > 1) {
+    textEnd = ` days ago.`;
+  }
+
+  lastVisit.textContent = `You last visted ` + differenceInDays + textEnd;
 }
