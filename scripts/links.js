53252a9b025028a7a2ea07bd5d404b9d3c61e8f6
davidakindileni <davidakindileni@yahoo.co.uk>
2023-07-18 00:48:54 +0100

    complete dynamic links

--- scripts/links.js
+++ scripts/links.js
@@ -2,13 +2,33 @@ const baseURL = "https://davidakindileni.github.io/wdd230/";
 const linksURL = "https://davidakindileni.github.io/wdd230/data/links.json";
 
 // get links from file
-async function getLinks() {
-  const response = await fetch(linksURL);
-  if (response.ok) {
-    const data = await response.json();
-    console.log(data);
-    console.log("data.week", data.week);
-    console.log("data.weeks", data.weeks);
-    console.log("data.weeks.week", data.weeks.week);
-  }
-}
+
+fetch(linksURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const weeks = jsonObject["weeks"];
+    let list = document.getElementById("weeks-links");
+
+    // loop through the week
+    for (let i = 0; i < weeks.length; i++) {
+      const weekNum = weeks[i].week; // get the week number
+      const links = weeks[i].links; // get the links in the week
+      let weekLinks = "";
+      weekLinks = weekLinks + weeks[i].week + `:  `; // add the week number to the week's list
+
+      // loop through the links in each week concatenating the links
+      for (let j = 0; j < links.length; j++) {
+        weekLinks =
+          weekLinks + `<a href="${links[j]["url"]}">${links[j]["title"]}</a>`;
+        if (j < links.length - 1) {
+          weekLinks = weekLinks + ` | `;
+        }
+      } // looping through links in each week ends
+
+      let item = document.createElement("li");
+      item.innerHTML = weekLinks;
+      list.appendChild(item);
+    } // looping through weeks ends
+  });
