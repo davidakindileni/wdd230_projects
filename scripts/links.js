1ee9823feaed31c4664bf6d85590c53a370486bf
davidakindileni <davidakindileni@yahoo.co.uk>
2023-07-17 18:35:38 +0100

    test links

--- scripts/links.js
+++ scripts/links.js
@@ -4,8 +4,11 @@ const linksURL = "https://davidakindileni.github.io/wdd230/data/links.json";
 // get links from file
 async function getLinks() {
   const response = await fetch(linksURL);
-  const data = await response.json();
-  console.log(data);
+  if (response.ok) {
+    const data = await response.json();
+    console.log(data);
+    // displayLinks(data.weeks);
+  }
 }
 
-getLinks();
+// getLinks();
