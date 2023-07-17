35d9fee0872a3803168a6dc19f6edf0c6380d3bc
davidakindileni <davidakindileni@yahoo.co.uk>
2023-07-17 13:42:22 +0100

    do dynamic links

--- scripts/homepage-scripts.js
+++ scripts/homepage-scripts.js
@@ -59,3 +59,38 @@ numVisits++;
 localStorage.setItem("numVisits-ls", numVisits);
 
 // 💡A client can view the localStorage data using the Applications panel in the browsers's DevTools - check it out on any major site.
+
+// Weather API
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?lat=7.47&lon=4.60&units=metric&APPID=012ba032305e851572d30ff7931ca4d2";
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      // console.log(data);
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp} &deg;C`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  const desc = data.weather[0].description;
+
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  captionDesc.textContent = desc;
+}
+apiFetch();
---
+++ scripts/links.js
@@ -0,0 +1,11 @@
+const baseURL = "https://davidakindileni.github.io/wdd230/";
+const linksURL = "https://davidakindileni.github.io/wdd230/data/links.json";
+
+// get links from file
+async function getLinks() {
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  console.log(data);
+}
+
+getLinks();
